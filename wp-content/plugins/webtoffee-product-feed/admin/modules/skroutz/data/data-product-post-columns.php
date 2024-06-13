21d59fcd0c9ae50edd323fa4a270608e848cb0ea
ironblodz <naylestorm12@gmail.com>
2024-06-13 23:08:08 +0100

    update

---
+++ wp-content/plugins/webtoffee-product-feed/admin/modules/skroutz/data/data-product-post-columns.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('WPINC')) {
+    exit;
+}
+
+$post_columns = array(
+                
+            'id'                           => 'Product Id[id]',
+            'title'                        => 'Product Title[title]',
+            'description'                  => 'Product Description[description]',
+            'link'                         => 'Product URL[link]',
+            'mobile_link'                  => 'Product URL[mobile_link]',
+            'product_type'                 => 'Product Categories[product_type] ',
+            'image_link'                   => 'Main Image[image_link]',
+            'additional_image_link'        => 'Additional Images [additional_image_link]',
+            'condition'                    => 'Condition[condition]',
+);
+
+return apply_filters('wt_pf_skroutz_product_post_columns',$post_columns);
