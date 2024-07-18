36ef37a4ebf71ade31826e7df6920ff5f3b6c993
ironblodz <naylestorm12@gmail.com>
2024-07-18 14:37:12 +0100

    backup

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1721914244;s:7:"authKey";s:64:"4Op]obw%NHTu5/#.>/uO))zJ[ZHax^TDSS!QJe1rG7}Jj]!&){0rG7m?U8rubx]O";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3578;}
